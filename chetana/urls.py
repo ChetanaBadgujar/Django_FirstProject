a0f01d8d0d0c988421f005d0a1b1a127e0aa4afa
ChetanaBadgujar <chetanabadgujar32548@gmail.com>
2022-07-18 23:04:50 +0530

    another commit

--- chetana/urls.py
+++ chetana/urls.py
@@ -20,6 +20,8 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 
+
+
 from django.views.static import serve
 from django.conf.urls import url
 
@@ -31,7 +33,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
 
 
-    url(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}), 
+    url(r'^media/(?P<path>.*)$', serve,{'document_root': settings.MEDIA_ROOT}), 
     url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
 ]
 
